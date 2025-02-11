05036dbca8e10a1ce871cfb321d3474f0dc74a81
tetsugo-mac <tetsugou2002@gmail.com>
2025-02-11 11:04:33 +0900

    GrommetIconsGithub.svgを削除し、GitHubリンクのアバター画像を外部URLに更新。Labリンクを新しいURLに変更。

--- src/components/organism/linksItems.tsx
+++ src/components/organism/linksItems.tsx
@@ -8,14 +8,17 @@ const linksItems = [
 	{
 		contents: (
 			<a href="https://github.com/tetsugo02" className={style.a}>
-				<Avatar src="/GrommetIconsGithub.svg" size="small" />
+				<Avatar
+					src="https://api.iconify.design/grommet-icons:github.svg?color=%23ababab"
+					size="small"
+				/>
 				<span className="ml-1">GitHub</span>
 			</a>
 		),
 	},
 	{
 		contents: (
-			<a href="https://lab.github.com" className={style.a}>
+			<a href="https://murata-lab.jp/" className={style.a}>
 				<Avatar src="https://api.iconify.design/ri:robot-2-line.svg?color=%23ababab" size="small" />
 				<span className="ml-1">Lab</span>
 			</a>
